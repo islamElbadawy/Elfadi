cac1c37d5544191ce5375da5513d2dc1b1f04231
islamElbadawy <islam.201211@gmail.com>
2021-11-23 16:39:43 +0200

    Our works slider done

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,29 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== Slider ====================*/ 
+
+var swiper = new Swiper(".gallery", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
